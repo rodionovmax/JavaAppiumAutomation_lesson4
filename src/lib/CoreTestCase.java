1215883ee17df4373a14c64c8454ca688794a7b2
rodionovmax <rodionovmax90test@gmail.com>
2018-12-22 16:35:26 -0500

    After lesson 4_1. Created java class CoreTestCase where moved transferred methods setUp() and tearDown

---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,41 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+//  TestCase is a method in junit
+public class CoreTestCase extends TestCase {
+
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+    @Override   //mentioning that we override parent method
+    public void setUp() throws Exception
+    {
+        super.setUp(); // mentioning that we use method setUp() from TestCase
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", "main.MainActivity");
+        capabilities.setCapability("app", "/Users/rodionovmax/IdeaProjects/JavaAppiumAutomation_homework1/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+    }
+
+    @Override
+    public void tearDown() throws Exception
+    {
+        driver.quit();
+
+        super.tearDown(); // mentioning that we use method tearDown() from TestCase
+    }
+}
